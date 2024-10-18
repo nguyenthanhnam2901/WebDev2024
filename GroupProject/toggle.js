3a5a869bedbf0d2e124248886d7ea0d2d5ebf2c0
Nguyễn Thành Nam <127769365+nguyenthanhnam2901@users.noreply.github.com>
2024-10-19 05:41:19 +0700

    Fix navigation bar

--- GroupProject/toggle.js
+++ GroupProject/toggle.js
@@ -1,9 +1,12 @@
-const bar = document.getElementById("bar")
-const menu = document.getElementById("menu")
-if(bar){
-    bar.addEventListener("click",()=>{
-        // console.log(3216574);
-        
-        menu.classList.toggle("active")
-    })
-}
+const bars = document.querySelectorAll('[id^="bar"]'); // Select all menu icons
+const menus = document.querySelectorAll('[id^="menu"]'); // Select all menus
+
+bars.forEach((bar, index) => {
+    const menu = menus[index]; // Get the corresponding menu by index
+
+    if (bar) {
+        bar.addEventListener("click", () => {
+            menu.classList.toggle("active");
+        });
+    }
+});
